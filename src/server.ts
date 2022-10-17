c210cc33f3df5e43e11a40a8a2c054b107ee7dcd
genesis-g101 <gyreplicant@gmail.com>
2022-10-17 00:18:53 -0300

    criando a estrutura base

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const server = express();
+const port = process.env.PORT;
 
-let out: string = 'hello world';
-console.log(out);
+server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.urlencoded({ extended: true }));
+
+server.use((req: Request, res: Response) => {
+  res.status(404).json({ error: '404 - not found' });
+});
 server.listen(3333, () => console.log('server ON: http://localhost:3333'));
