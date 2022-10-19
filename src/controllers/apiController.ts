6cc27ebbd552b5507049479fdb52456664efbe01
genesis-g101 <gyreplicant@gmail.com>
2022-10-19 11:04:00 -0300

    atualizando e deletando frases

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from 'express';
-import { Phrase } from '../models/Phrase';
+import { Phrase, PhraseModel } from '../models/Phrase';
+
+export const createPhrase = async (req: Request, res: Response) => {
+  const { author, text }: PhraseModel = req.body;
+
+  const newPhrase = await Phrase.create({ author, text });
+  res.status(201).json({ msg: newPhrase });
+};
 
 export const getPhrases = async (req: Request, res: Response) => {
   const phrases = await Phrase.findAll();
@@ -7,24 +14,43 @@ export const getPhrases = async (req: Request, res: Response) => {
 };
 export const getPhrasesByAuthor = async (req: Request, res: Response) => {
   const { name } = req.query;
-  console.log(req.query);
+
   const phrasesByAuthor = await Phrase.findAll({ where: { author: name }, attributes: ['text'] });
   res.status(200).json({ phrasesByAuthor });
 };
 
 export const getPhraseById = async (req: Request, res: Response) => {
   const { id } = req.params;
-  console.log(id);
-  const phrase = await Phrase.findByPk(id, { attributes: ['id', 'text'] });
+
+  const phrase = await Phrase.findByPk(id);
   if (!phrase) {
     res.status(404).json({ error: 'frase não encontrada' });
   }
   res.status(200).json({ phrase });
 };
 
-export const createPhrase = async (req: Request, res: Response) => {
-  const { author, text }: { author: string; text: string } = req.body;
+export const updatePhrase = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { author, text } = req.body;
+  const phrase = await Phrase.findByPk(id);
+  if (!phrase) {
+    res.status(404).json({ error: 'frase não encontrada' });
+  }
+  phrase!.author = author;
+  phrase!.text = text;
 
-  const newPhrase = await Phrase.create({ author, text });
-  res.status(201).json({ msg: newPhrase });
+  await phrase?.save();
+  res.status(200).json({ msg: 'updated', phrase });
+};
+
+export const deletePhrase = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const phrase = await Phrase.findByPk(id);
+  if (!phrase) {
+    res.status(404).json({ error: 'frase não encontrada' });
+  }
+  await Phrase.destroy({ where: { id: id } });
+
+  res.json({});
 };
--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,13 @@ import * as apiController from './controllers/apiController';
 
 const router = Router();
 
+router.post('/frases', apiController.createPhrase);
+
 router.get('/frases', apiController.getPhrases);
 router.get('/frases/author', apiController.getPhrasesByAuthor);
-router.get('/frase/:id', apiController.getPhraseById);
+router.get('/frases/:id', apiController.getPhraseById);
 
-router.post('/frases', apiController.createPhrase);
+router.put('/frases/:id', apiController.updatePhrase);
+router.delete('/frases/:id', apiController.deletePhrase);
 
 export default router;
